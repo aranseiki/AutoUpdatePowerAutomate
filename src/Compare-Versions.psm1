7da11fdaaee3f0185befb23e3d7f03806ad564ec
Allan de Oliveira Almeida <aoalmeida2@stefanini.com>
2024-10-11 15:19:10 -0300

    Fix in the Compare-Versions file

--- src/Compare-Versions.psm1
+++ src/Compare-Versions.psm1
@@ -1,16 +1,29 @@
 ﻿Function Compare-Versions {
     Param(
-        [string] $CurrentVersion,
-        [string] $InstallFileVersion
+        [System.Version] $CurrentVersion,
+        [System.Version] $InstallFileVersion,
+        [int] $Level
     )
-    $CurrentVersionWithoutBuild = (
-        ($CurrentVersion -isplit '\.') | Select-Object -First 3
-    ) -join '.'
-    $InstallFileVersionWithoutBuild = (
-        ($InstallFileVersion -isplit '\.') | Select-Object -First 3
-    ) -join '.'
 
-    return ($CurrentVersionWithoutBuild, $InstallFileVersionWithoutBuild)
+    if ($Level -lt 1 -or $Level -gt 4) {
+        Write-Error 'Level value invalid. Try a number between 1 and 4'
+    }
+    
+    $CurrentVersionFormatted = $CurrentVersion.ToString($Level)
+    $InstallFileVersionFormatted = $InstallFileVersion.ToString($Level)
+    
+    $ComparisonResult = ''
+    If ($CurrentVersionFormatted -lt $InstallFileVersionFormatted) {
+        $ComparisonResult = 'Newer version available, proceeding with update.'
+    }
+    ElseIf ($CurrentVersionFormatted -eq $InstallFileVersionFormatted) {
+        $ComparisonResult = 'The current version is up to date. No update is needed.'
+    }
+    ElseIf ($CurrentVersionFormatted -gt $InstallFileVersionFormatted) {
+        $ComparisonResult = 'The current version is newer than the install file version. No action taken.'
+    }
+
+    return $ComparisonResult
 }
 
 Export-ModuleMember -Function Compare-Versions
